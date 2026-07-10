639fa3fbbc498c98ca45ce4e4f16e09e12e0b691
Halabenio <beniamin.halaburda@halabenio.com>
2026-07-10 10:11:06 +0100

    Debugging

--- main.ts
+++ main.ts
@@ -4,98 +4,100 @@ enum RadioMessage {
     CarKeepalive = 29201,
     ControllerKeepalive = 30085
 }
-radio.onReceivedMessage(RadioMessage.CarKeepalive, function () {
-    Connected = -1
-    bitbot.setLedColor(0xFF0000)
-    if (LastKeepalive <= 50) {
-        LastKeepalive = 50
-        music.play(music.stringPlayable("F C5 - - - - - - ", 500), music.PlaybackMode.InBackground)
-    }
-})
-radio.onReceivedMessage(RadioMessage.ControllerKeepalive, function () {
-    Connected = 1
-    bitbot.setLedColor(0x8000FF)
-    if (LastKeepalive <= 50) {
-        LastKeepalive = 50
-    }
-})
 input.onButtonPressed(Button.A, function () {
-    Connected = -1
-    if (Channel > 0) {
-        Channel += -1
-    } else {
-        Channel = 9
+    if (Status == 1) {
+        SettingNumber += -1
+        if (SettingNumber < 0) {
+            SettingNumber = 20
+        }
+        basic.showNumber(SettingNumber)
     }
-    basic.showNumber(Channel)
-    radio.setGroup(40 + Channel)
-    bitbot.setLedColor(0xFFC000)
-    Connected = -1
 })
 input.onButtonPressed(Button.AB, function () {
-    music.play(music.stringPlayable("E C F - - C5 - - ", 500), music.PlaybackMode.InBackground)
-    Connected = 0
-    bitbot.ledRainbow(false, BBArms.Left)
-    bitbot.ledRainbow(false, BBArms.Right)
-})
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == "Left") {
-        bitbot.move(BBMotor.Left, BBDirection.Forward, 60)
-    }
-    if (receivedString == "Right") {
-        bitbot.move(BBMotor.Right, BBDirection.Forward, 60)
-    }
-    if (receivedString == "StopLeft") {
-        bitbot.move(BBMotor.Left, BBDirection.Forward, 0)
-    }
-    if (receivedString == "StopRight") {
-        bitbot.move(BBMotor.Right, BBDirection.Forward, 0)
+    if (Status == 0) {
+        Status = 1
+        SettingNumber = parseFloat(flashstorage.getOrDefault("Gateway", "0"))
+        basic.showNumber(SettingNumber)
+        bitbot.setLedColor(0xFF8000)
+    } else if (Status == 1) {
+        Gateway = SettingNumber
+        flashstorage.put("Gateway", convertToText(SettingNumber))
+        radio.setGroup(Gateway * 10)
+        serial.writeNumber(Gateway * 10)
+        Status = 2
+        bitbot.setLedColor(0x0000FF)
+        radio.sendValue("CarAwait", 0)
+    } else if (Status == 2) {
+        Status = 1
+        SettingNumber = parseFloat(flashstorage.getOrDefault("Gateway", "0"))
+        basic.showNumber(SettingNumber)
+        bitbot.setLedColor(0xFF8000)
+    } else if (Status == 3) {
+    	
     }
 })
 input.onButtonPressed(Button.B, function () {
-    Connected = -1
-    if (Channel < 9) {
-        Channel += 1
-    } else {
-        Channel = 0
-    }
-    basic.showNumber(Channel)
-    radio.setGroup(40 + Channel)
-    bitbot.setLedColor(0xFFC000)
-    Connected = -1
-})
-let LastKeepalive = 0
-let Channel = 0
-let Connected = 0
-Connected = -1
-Channel = 0
-basic.showNumber(Channel)
-radio.setGroup(40)
-bitbot.setLedColor(0xFFC000)
-music.play(music.stringPlayable("C E G - - - - - ", 500), music.PlaybackMode.InBackground)
-loops.everyInterval(50, function () {
-    if (Connected == -1) {
-        LastKeepalive += -1
-        if (LastKeepalive <= 0) {
-            bitbot.setLedColor(0xFFC000)
+    if (Status == 1) {
+        SettingNumber += 1
+        if (SettingNumber > 20) {
+            SettingNumber = 0
         }
-    } else if (Connected == 1) {
-        LastKeepalive += -1
-        if (LastKeepalive <= 0) {
-            Connected = 0
-            bitbot.ledRainbow(false, BBArms.Left)
-            bitbot.ledRainbow(false, BBArms.Right)
-        }
+        basic.showNumber(SettingNumber)
     }
 })
-basic.forever(function () {
-    if (Connected == 0) {
-        radio.sendMessage(RadioMessage.CarKeepalive)
-        basic.showNumber(Channel)
-        bitbot.move(BBMotor.Left, BBDirection.Forward, 0)
-        bitbot.move(BBMotor.Right, BBDirection.Forward, 0)
-    } else if (Connected == 1) {
-        radio.sendMessage(RadioMessage.CarKeepalive)
+radio.onReceivedValue(function (name, value) {
+    serial.writeValue(name, value)
+    basic.showIcon(IconNames.Heart)
+    if (name == "CarDispatch") {
+        basic.showIcon(IconNames.Happy)
+        Status = 3
+        bitbot.setLedColor(0x18E600)
+        radio.setGroup(Gateway * 10 + value)
+        serial.writeNumber(Gateway * 10 + value)
+        Address = Gateway * 10 + value
+    } else if (name == "GatewayFull") {
+        basic.showIcon(IconNames.Sad)
+        Status = 4
+        for (let index = 0; index < 8; index++) {
+            bitbot.setPixelColor(0, 0xff0000)
+            bitbot.ledRotate(true, BBArms.Both)
+            bitbot.ledRotate(true, BBArms.Both)
+        }
+    } else if (name == "IsCar") {
+        radio.sendValue("CarHere", 0)
+    } else if (name == "DrivePacket") {
+        bitbot.move(BBMotor.Left, BBDirection.Forward, parseFloat(convertToText(value).substr(0, 3)))
+        bitbot.move(BBMotor.Right, BBDirection.Forward, parseFloat(convertToText(value).substr(3, 3)))
+        radio.sendValue("Driving", 0)
+    } else if (name == "CarHere") {
+        radio.setGroup(Gateway * 10)
+        radio.sendValue("DoubleCar", Address)
     }
-    bitbot.ledRotate(true, BBArms.Left)
-    bitbot.ledRotate(false, BBArms.Right)
+})
+let Address = 0
+let SettingNumber = 0
+let Gateway = 0
+let Status = 0
+Status = 0
+// Unset
+basic.showLeds(`
+    # # . # #
+    . . . . .
+    . # # # .
+    # # # # #
+    . # . # .
+    `)
+bitbot.setLedColor(0xff0000)
+if (flashstorage.getOrDefault("Gateway", "NotSet") != "NotSet") {
+    Status = 2
+    Gateway = parseFloat(flashstorage.getOrDefault("Gateway", "0"))
+    bitbot.setLedColor(0x0000FF)
+    radio.setGroup(Gateway * 10)
+    serial.writeNumber(Gateway * 10)
+    radio.sendValue("CarAwait", 0)
+}
+loops.everyInterval(500, function () {
+    if (Status == 2 || Status == 4) {
+        radio.sendValue("CarAwait", 0)
+    }
 })
